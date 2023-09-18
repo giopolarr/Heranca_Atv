e0d10846a2b57a29807dec16b8cb1fdfeb9769aa
giopolarr <giovannasimionero@gmail.com>
2023-09-18 16:25:32 -0300

    second commit

--- Heranca_Ativi/Program.cs
+++ Heranca_Ativi/Program.cs
@@ -1,6 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Linq.Expressions;
 using System.Text;
 using System.Threading.Tasks;
 
@@ -11,20 +12,73 @@ namespace Heranca_Ativi
         static void Main(string[] args)
         {
             Conta conta = new Conta(11, "120", "Giovanna", 150);
-
-
-            Console.WriteLine("Digite o valor do saque: ");
-            double valorSaquee = Convert.ToDouble(Console.ReadLine());
-            conta.Sacar(valorSaquee);
+           
+            while (true) 
             
+            {
+                Console.WriteLine("Qual conta você quer: " +
+                    "\n para estudantil digite 1" +
+                    " \n para empresarial digite 2 " +
+                    "\n Apenas abrir conta digite 3");
+                int tipoConta = Convert.ToInt32(Console.ReadLine());
 
-            Console.WriteLine("Digite o valor do deposito: ");
-            double valorDeposito = Convert.ToDouble(Console.ReadLine());
-            conta.Depositar(valorDeposito);
-           Console.WriteLine(valorDeposito);
+                try
+                {
+                    if (tipoConta == 1)
+                    {
+                        Console.WriteLine("Para sacar digite: 3 \n Para depositar digite: 4");
+                        int escolha = Convert.ToInt32(Console.ReadLine());
+                        if (escolha == 3)
+                        {
+                            Console.WriteLine("Digite o valor do saque: ");
+                            double valorSaquee = Convert.ToDouble(Console.ReadLine());
+                            conta.Sacar(valorSaquee);
+                        }
+                        else { Console.WriteLine("Erro"); }
+                    }
+                    if(tipoConta == 2)
+                    {
+                        Console.WriteLine("Para sacar digite: 1 \n Para fazer emprestimo digite:2");
+                        int escolha = Convert.ToInt32((Console.ReadLine()));
+                        if (escolha == 1)
+                        {
+                            Console.WriteLine("Digite o valor do saque: ");
+                            double valorSaquee = Convert.ToDouble(Console.ReadLine());
+                            
+                        } else { Console.WriteLine("erro"); }
+                        if(escolha == 2)
+                        {
+                            Console.WriteLine("Digite o valor do Emprestimo ");
+                            double valorEmprestimo = Convert.ToDouble(Console.ReadLine());
 
+                        }
+                    }
+                    if(tipoConta == 3)
+                    {
+                        Console.WriteLine("Para sacar digite: 1 \n Para fazer um deposito digite:2");
+                        int escolha = Convert.ToInt32((Console.ReadLine()));
+                        if(escolha == 1)
+                        {
+                            Console.WriteLine("Digite o valor do saque: ");
+                            double valorSaquee = Convert.ToDouble(Console.ReadLine());
+                            conta.Sacar(valorSaquee);
 
+                        }
+                        if(escolha == 2)
+                        {
+                            Console.WriteLine("Digite o valor do deposito: ");
+                            double valorDeposito = Convert.ToDouble(Console.ReadLine());
+                            conta.Depositar(valorDeposito);
+                            Console.WriteLine(valorDeposito);
+                        }
+                    }
+                }
+                catch (Exception e)
+                {
 
+                    Console.WriteLine("Erro");
+                }
+            }
             Console.ReadKey();
         }
     }
